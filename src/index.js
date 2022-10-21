124b6e353db500b1d14abe2f7dca60a1ef93fcd0
Yaroslav <25lizer25@gmail.com>
2022-10-21 15:18:47 +0200

    add scroll.js and var --scrollTop in css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 //мониторинг файлов
 import './index.html';
 import './index.scss';
-import { mult, sum } from './modules/calc';
-
-//хуйня
-console.log(mult(2, 4));
-console.log(sum(3,3));
+import './modules/scroll';
--- src/modules/calc.js
+++
@@ -1,2 +0,0 @@
-export const mult = (a, b) => a * b;
-export const sum = (a, b) => a + b;
